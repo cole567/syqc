498f00c95cb671b2513182124f9bce0c603e8b10
cole567 <cole666521@gmail.com>
2025-11-26 18:58:07 +0800

    Fix formatting and comments in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.') 
+      '@': path.resolve(__dirname, '.'),
     }
   },
-  // 👇👇👇 新增这一段，专门解决 "process is not defined" 错误 👇👇👇
+  // 必须加上这个，防止 "process is not defined" 报错
   define: {
-    'process.env': {}
+    'process.env': {} 
   }
-})
+});
